253381e172f45feb64d8008763b09204814db9ee
Gamer64ytb <76565986+Gamer64ytb@users.noreply.github.com>
2022-04-12 16:42:55 +0200

    Android: Finish EmulationActivity closing a game.
    This also gives a boost closing a game.

--- Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
+++ Source/Android/app/src/main/java/org/dolphinemu/dolphinemu/activities/EmulationActivity.java
@@ -724,6 +724,7 @@ public final class EmulationActivity extends AppCompatActivity
 
       case MENU_ACTION_EXIT:
         mEmulationFragment.stopEmulation();
+        finish();
         break;
     }
   }
